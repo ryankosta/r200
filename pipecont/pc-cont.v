4c930325c26aba053702739a705444e5d9b6b9a2
Ryan Kosta <ryanpkosta@gmail.com>
2021-04-24 14:53:00 -0700

    pc-cont: switches to blocking assignments
    blocking assignments are needed as this is a non-clock-driven block.

--- pipecont/pc-cont.v
+++ pipecont/pc-cont.v
@@ -22,34 +22,34 @@ output reg [31:0] pcp4_hold;
 output reg if_id_retire; //retire instruction passed into if/id reg at next posedge clk
 always @(*) begin
 	if(rst) begin
-		pcsel <= 0;
-		if_id_retire <= 0;
+		pcsel = 0;
+		if_id_retire = 0;
 	end
 	else if(id_jmp) begin
-		if_id_retire <= 1;
+		if_id_retire = 1;
 	end
 	else if(id_isbr) begin
-		if_id_retire <= 1;
-		pcsel <= 1; //specutively branch
-		pcp4_hold <= pcp4; //hold pcp4 incase code doesnt branch 
+		if_id_retire = 1;
+		pcsel = 1; //specutively branch
+		pcp4_hold = pcp4; //hold pcp4 incase code doesnt branch 
 	end
 	else if(ex_isbr) begin
 		if(ex_willbr) begin
-			if_id_retire <= 0; //since specutively branched current instruction in IF is valid
-			pcsel <= 0; //since branch addr already set in previous stage
+			if_id_retire = 0; //since specutively branched current instruction in IF is valid
+			pcsel = 0; //since branch addr already set in previous stage
 		end	
 		else begin
-			if_id_retire <= 1;
-			pcsel <= 3; //pc = pchold
+			if_id_retire = 1;
+			pcsel = 3; //pc = pchold
 		end	
 	end
 	else if(ex_jmp) begin
-		if_id_retire <= 1;
-		pcsel <= 2;
+		if_id_retire = 1;
+		pcsel = 2;
 	end
 	else begin
-		if_id_retire <= 0;
-		pcsel <= 0;
+		if_id_retire = 0;
+		pcsel = 0;
 	end
 end
 endmodule
